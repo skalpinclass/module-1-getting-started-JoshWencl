9f37c778a649528be16ce2158600dcdba8fa1ef1
Joshua Wencl <joshw@magenic.com>
2019-07-24 10:23:46 -0500

    Added homework.

---
+++ ConsoleBlog/ConsoleBlog/Program.cs
@@ -0,0 +1,24 @@
+﻿using Intro;
+using System;
+
+namespace ConsoleBlog
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var db = new BloggingContext())
+            {
+                db.Database.EnsureCreated();
+                var blog = new Blog { Url = "http://sample.com" };
+                db.Blogs.Add(blog);
+                db.SaveChanges();
+
+                foreach (var blogEntry in db.Blogs)
+                {
+                    Console.WriteLine($"{blog.BlogId}");
+                }
+            }
+        }
+    }
+}
